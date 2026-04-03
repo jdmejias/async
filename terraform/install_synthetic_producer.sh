54dab109baf044b28c7425e9af7a09da2b96da7e
unknown <nestorortiz017@gmail.com>
2026-04-03 18:47:13 -0500

    removed iam requests

--- terraform/install_synthetic_producer.sh
+++ terraform/install_synthetic_producer.sh
@@ -2,10 +2,10 @@
 # Amazon Linux 2023 - Instalar y ejecutar Synthetic Producer
 sudo dnf update -y
 sudo dnf install -y python3 python3-pip git
-pip3 install boto3
 
 APP_DIR="/home/ec2-user/async"
 REPO_URL="${repo_url}"
+API_IP="${api_ip}"
 
 if [ ! -d "$APP_DIR" ]; then
   git clone "$REPO_URL" "$APP_DIR"
@@ -13,14 +13,8 @@ fi
 
 cd "$APP_DIR" || exit 1
 
-API_IP=$(python3 - <<'PY'
-from get_parameter import get_ssm_parameter
-print(get_ssm_parameter("/message-queue/dev/api/public_ip", "localhost"))
-PY
-)
-
 pip3 install -r requirements.txt
 
 pkill -f "python3 synthetic_producer.py" >/dev/null 2>&1 || true
-nohup env API_BASE_URL="http://$${API_IP}:8000" PRODUCER_INTERVAL_SECONDS="10" \
+nohup env API_BASE_URL="http://$API_IP:8000" PRODUCER_INTERVAL_SECONDS="10" \
   python3 synthetic_producer.py > /home/ec2-user/synthetic_producer.log 2>&1 &
--- terraform/install_worker.sh
+++ terraform/install_worker.sh
@@ -2,10 +2,11 @@
 # Amazon Linux 2023 - Instalar y ejecutar Worker
 sudo dnf update -y
 sudo dnf install -y python3 python3-pip git
-pip3 install boto3
 
 APP_DIR="/home/ec2-user/async"
 REPO_URL="${repo_url}"
+RABBITMQ_IP="${rabbitmq_ip}"
+POSTGRES_IP="${postgres_ip}"
 
 if [ ! -d "$APP_DIR" ]; then
 	git clone "$REPO_URL" "$APP_DIR"
@@ -13,21 +14,9 @@ fi
 
 cd "$APP_DIR" || exit 1
 
-RABBIT_IP=$(python3 - <<'PY'
-from get_parameter import get_ssm_parameter
-print(get_ssm_parameter("/message-queue/dev/rabbitmq/public_ip", "localhost"))
-PY
-)
-
-POSTGRES_IP=$(python3 - <<'PY'
-from get_parameter import get_ssm_parameter
-print(get_ssm_parameter("/message-queue/dev/postgres/public_ip", "localhost"))
-PY
-)
-
 cat > .env.aws <<EOF
-DATABASE_URL=postgresql://appuser:password@$${POSTGRES_IP}:5432/appdb
-RABBIT_URL=amqp://guest:guest@$${RABBIT_IP}:5672/
+DATABASE_URL=postgresql://appuser:password@$POSTGRES_IP:5432/appdb
+RABBIT_URL=amqp://guest:guest@$RABBITMQ_IP:5672/
 EXCHANGE=tasks.exchange
 QUEUE=tasks.queue
 EOF
